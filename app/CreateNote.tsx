4a386e93e43ccc83c71dca4e904807078c568ed3
Andre Paulino <andrepaulinobtw@gmail.com>
2023-05-15 20:40:52 -0300

    first commit

---
+++ app/CreateNote.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function CreateNote() {
+	const [title, setTitle] = useState("");
+	const [content, setContent] = useState("");
+
+	const router = useRouter();
+
+	const create = async () => {
+		await fetch("http://127.0.0.1:8090/api/collections/notes/records/", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ title, content }),
+		});
+
+		setTitle("");
+		setContent("");
+
+		router.refresh();
+	};
+
+	return (
+		<form onSubmit={create}>
+			<h3>Create a new note</h3>
+			<input
+				type="text"
+				placeholder="Title"
+				value={title}
+				onChange={(event) => setTitle(event.target.value)}
+			/>
+			<textarea
+				placeholder="Content"
+				value={content}
+				onChange={(event) => setContent(event.target.value)}
+			></textarea>
+			<button type="submit">Create Note</button>
+		</form>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,43 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from "next/link";
+import styles from "./Notes.module.css";
+import CreateNote from "./CreateNote";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+async function getNotes() {
+	const response = await fetch(
+		"http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30",
+		{ cache: "no-store" }
+	);
+	const data = await response.json();
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	return data?.items as any[];
+}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+export default async function NotesPage() {
+	const notes = await getNotes();
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+	return (
+		<div>
+			<h1>Notes</h1>
+			<div className={styles.grid}>
+				{notes?.map((note) => {
+					return <Note key={note.id} note={note} />;
+				})}
+			</div>
+			<CreateNote />
+		</div>
+	);
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+function Note({ note }: any) {
+	const { id, title, content, created } = note || {};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<Link href={`/${id}`}>
+			<div className={styles.note}>
+				<h2>{title}</h2>
+				<h5>{content}</h5>
+				<p>{created}</p>
+			</div>
+		</Link>
+	);
 }
